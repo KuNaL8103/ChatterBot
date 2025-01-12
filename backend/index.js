f19716cb27e857fc3497313775e4c39d387da1d9
Kunal Dabbe <153227163+KuNaL8103@users.noreply.github.com>
2025-01-12 12:58:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -156,10 +156,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../../client")));
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.listen(port, () => {
